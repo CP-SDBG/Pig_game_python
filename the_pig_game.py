e992d2614c8f9055d2094dc60e1c6ea59683a115
CP-SDBG <123507013+CP-SDBG@users.noreply.github.com>
2024-09-17 07:47:46 +0200

    added player turn function

--- the_pig_game.py
+++ the_pig_game.py
@@ -3,5 +3,18 @@ import random
 def rollDie():
     return random.randint(1,6)
 
-
-print(rollDie())
+def play_turn(player_name):
+    """Simulate a  turn for a player."""
+    turn_total = 0
+    while True:
+        roll = roll_die()
+        print(f"{player_name} rullede: {roll}")
+        if roll == 1:
+            print(f"{player_name} mistede alle point for denne runde!\n")
+            return 0
+        else:
+            turn_total += roll
+            print(f"{player_name}'s foreløbige score for denne runde: {turn_total}")
+            response = input("Vil du rulle igen? (y/n) ")
+            if response.lower() != 'y':
+                return turn_total
